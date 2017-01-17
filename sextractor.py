15a496814f04c9e138a9601552bff0d5fe71f000
rzemaitis <rokas.zemaitis2@gmail.com>
2017-01-17 15:01:47 +0000

    File reader, targetfind prototype

--- sextractor.py
+++ sextractor.py
@@ -1,5 +1,30 @@
+import numpy as np
+
+def targetfind():
+	var = raw_input("Please find your object and write its number:")
+	print var
+
+#Set filename
+filename="../Exoplanets1/test.cat"
+#Create a line array
+lines=[]
 #Read lines without comments
-with open("filename") as f:
-    for line in f:
-        line = line.split('#', 1)[0]
-        line = line.rstrip()
+with open(filename) as f:
+	for line in f:
+		if not line.startswith("#"):
+			line=line.rstrip('\n')
+			#print line.rstrip()
+			lines.append(line)
+#Create array for storing data
+data=[]
+
+#Split lines into values, and put them into
+for i in range (0,len(lines)-1):
+	a=lines[i].split()
+	data.append(a)
+print data[0][1]
+
+
+targetfind()
+#for i in range (0,len(lines)-1):
+#	data[i][]=chars[i]
