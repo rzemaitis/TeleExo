61455eb2b6b54c73c78e9002a27f659832c4e585
rzemaitis <rokas.zemaitis2@gmail.com>
2017-02-22 22:41:39 +0000

    Cutter and tadder for probably last changes to the fitting folks.

--- cutter.py
+++ cutter.py
@@ -7,7 +7,8 @@ import math
 import signal
 from colorama import init
 from colorama import Fore, Back, Style
-
+#Init colorama
+init(autoreset=True)
 
 #Set all filenames
 #Set path
@@ -18,9 +19,14 @@ namebase="Target_star"
 nameroot=".txt"
 timebase="Time"
 xmagbase="XMag"
-
 magid=1
-
+#Stars, including target star
+stars=4
+#Exclude these images at specific times
+excl=[5052,5064]
+#Threshold
+thresh=0.12
+#excl=[]
 #Create a filename
 fname=namepath+namebase+nameroot
 #Read lines without comments
@@ -31,6 +37,14 @@ with open(fname) as f:
 			line=line.rstrip('\n')
 			lines.append(line)
 f.close()
+#Read time for exclusion
+timefile = open(namepath+timebase+nameroot,"r")
+tlines = timefile.readlines()
+timefile.close()
+time=[]
+for i in range (0,len(tlines)):
+	a=tlines[i].split()
+	time.append(a)
 #Split lines into values, and put them into data array
 data=[]
 for i in range (0,len(lines)):
@@ -42,35 +56,56 @@ mask=[]
 #Index stopper to get rid of more than one bad dip
 stuck=0
 
-for i in range(len(data)):
-	if not i==0:
-		newmag=float(data[i][magid])
-		oldmag=float(data[i-1][magid])
-		if not stuck==0:
-			oldmag=float(data[stuck][magid])
-		if(abs(newmag-oldmag)>0.25 and newmag>oldmag):
-			print("Image= %d "%i)
+for i in range(len(lines)):
+	dropcheck=False
+	for j in range(0,stars):
+		if i==0:
+			mask.append(True)
+			dropcheck=True
+			break
+		#print str(magid+j*2)
+		if stuck==0:
+			newmag=float(data[i][magid+j*2])
+			oldmag=float(data[i-1][magid+j*2])
+		else:
+			newmag=float(data[i][magid+j*2])
+			oldmag=float(data[stuck][magid+j*2])
+		#print "Excl time "+str(excl).strip('[]')+" Our time "+str(time[i][0])
+		if int(time[i][0]) in excl:
+			print Fore.BLUE+"Found excl!"
 			mask.append(False)
+			dropcheck=True
+			break
+		if(abs(newmag-oldmag)>thresh and newmag>oldmag):
+			#print("Image= %d false"%i)
+			mask.append(False)
+			#print "Newmag "+str(newmag)+" Oldmag "+str(oldmag)
 			if stuck==0:				
 				stuck=i-1
-		else:
-			mask.append(True)
-			stuck=0
-	else:
+			#print ("i is at "+str(i)+" j is at "+str(j))
+			dropcheck=True
+			break
+	if not dropcheck:
+		stuck=0
+	#	print("Image= %d true"%i)
 		mask.append(True)
+	#print "Mask is now "+str(len(mask))+" long while we're at i %d"%(i+1)
+
 #Write new file
 starfile = open(namepath+namebase+"_trim"+nameroot,"w")
 for i in range(len(lines)):
 	if(mask[i]):
+		#print data[i][magid]
 		starfile.write(lines[i]+'\n')
+'''	else:
+		print "No"
+		print str(mask[i])+" "+str(i)
+		print data[i][magid]'''
 #Cut time file
-timefile = open(namepath+timebase+nameroot,"r")
-lines = timefile.readlines()
-timefile.close()
 timefile = open(namepath+timebase+"_trim"+nameroot,"w")
 for i in range(len(lines)):
 	if(mask[i]):
-		timefile.write(lines[i])
+		timefile.write(tlines[i])
 
 #Cut xmag file
 xmagfile = open(namepath+xmagbase+nameroot,"r")
@@ -80,7 +115,6 @@ xmagfile = open(namepath+xmagbase+"_trim"+nameroot,"w")
 for i in range(len(lines)):
 	if(mask[i]):
 		xmagfile.write(lines[i])
-
 timefile.close()
 starfile.close()
 xmagfile.close()
