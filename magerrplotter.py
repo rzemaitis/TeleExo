263a590c5f2373668c7e7128328ac122c49a892e
rzemaitis <rokas.zemaitis2@gmail.com>
2017-02-23 11:12:43 +0000

    XMag file wrong. Need repairs.

---
+++ magerrplotter.py
@@ -0,0 +1,67 @@
+import subprocess
+import os
+import sys
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+#Paths
+targetpath ="./Data/"
+targetname="Qatar1b.txt"
+fname=targetpath+targetname
+lines=[]
+
+stars=1
+magid=1
+magerrid=2
+
+#Read lines without comments
+with open(fname) as f:
+	for line in f:
+		if not line.startswith("#"):
+			line=line.rstrip('\n')
+			lines.append(line)
+f.close()
+data=[]
+#Split lines into values, and put them into data array
+for i in range (0,len(lines)):
+	a=lines[i].split()
+	data.append(a)
+#Make lists for storing x, y and yerr
+#print data
+time=np.zeros(len(lines))
+mag=[]
+magerr=[]
+for i in range(0,stars):
+	mags=np.zeros(len(lines))
+	magerrs=np.zeros(len(lines))
+	for j in range(len(lines)):
+		#print data[j][magid+(i+1)*2]
+		mags[j]=float(data[j][magid+(i)])
+		magerrs[j]=float(data[j][magerrid+(i)*2])
+	mag.append(mags)
+	magerr.append(magerrs)
+for i in range(len(lines)):
+	time[i]=float(data[i][0])
+# Plot values
+for i in range(0,stars):
+	plt.errorbar((time[:]),(mag[i][:]),yerr=magerr[i][:],fmt='o',zorder=1)
+	plt.plot((time[:]),(mag[i][:]),zorder=0)
+#for i in range(0,stars):
+#	if i==0:
+#		plt.plot((time[:]),(mag[i][:]))
+#	else:
+#		plt.plot((time[:]),(mag[i][:]),'--')
+# control the axis range
+#plt.xlim(5.5, 18.5)
+#plt.ylim(0, 8.0)
+title_font = {'size':'40', 'color':'black','weight':'normal','verticalalignment':'bottom'} 
+axis_font = {'size':'28'}
+plt.xlabel('Time (seconds)', **axis_font)
+plt.ylabel('Magnitude', **axis_font)
+plt.title('Lightcurve (mean)', **title_font)
+plt.xticks(fontsize=15)    # fontsize of the tick labels
+plt.yticks(fontsize=15)    # fontsize of the tick labels
+#plt.gca().set_color_cycle(["red", "green", "blue"])
+plt.gca().invert_yaxis()
+plt.show()
